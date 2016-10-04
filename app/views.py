0ba9f2da482104c270942b7f5c80921fe249abf1
David Ficociello <david+github@gmail.com>
2016-10-04 12:07:07 -0400

    fixed main artist page. now can search via form

--- app/views.py
+++ app/views.py
@@ -1,11 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import requests
 import json
 import time
-import ast
 import grequests
-from pprint import pprint
+# from pprint import pprint
 DEBUG = False
 REL_DEBUG = False
 IMG_DEBUG = False
@@ -15,7 +14,10 @@ PREVIEW_DEBUG = True
 def get_related_artists(artist_id):
     if REL_DEBUG:
         start = time.time()
-    request_string = 'https://api.spotify.com/v1/artists/' + artist_id + '/related-artists'
+    request_string = 'https://api.spotify.com/v1/artists/' \
+        + artist_id \
+        + '/related-artists'
+
     r = requests.get(request_string)
     related_artist_list = []
     full_list = r.json()['artists']
@@ -27,22 +29,13 @@ def get_related_artists(artist_id):
         print('get_related_artists took {} seconds'.format(total_time))
     return full_list
 
+
 def load_preview_track_url(artist_id):
     # payload = {'country': 'US'}
-    return 'https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks?country=US'
+    return 'https://api.spotify.com/v1/artists/' \
+        + artist_id \
+        + '/top-tracks?country=US'
 
-def get_preview_track(artist_id):
-    if PREVIEW_DEBUG:
-        start = time.time()
-    payload = {'country': 'US'}
-    request_string = 'https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks'
-    r = requests.get(request_string, params=payload)
-    if PREVIEW_DEBUG:
-        end = time.time()
-        total_time = end - start
-        print('get_preview_track took {} seconds'.format(total_time))
-
-    return r.json()['tracks'][0]['preview_url']
 
 def get_artist_id(artist_name):
     if DEBUG:
@@ -60,13 +53,14 @@ def get_artist_id(artist_name):
 
 def get_preview_tracks_async(preview_tracks):
     urls = preview_tracks
-    rs = (grequests.get(u) for u in urls )
+    rs = (grequests.get(u) for u in urls)
     preview_urls = []
     for response in grequests.map(rs):
         str_resp = response.content.decode("utf-8")
         json_data = json.loads(str_resp)
         preview_urls.append(json_data['tracks'][0]['preview_url'])
     return preview_urls
+
 
 def get_artist_image(artist_id, image_num=1):
     if IMG_DEBUG:
@@ -85,27 +79,36 @@ def get_artist_image(artist_id, image_num=1):
 
     return r.json()['images'][image_num]['url']
 
-@app.route('/',methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/artist/<name>',methods=['GET', 'POST'])
+
+@app.route('/artist', methods=['GET', 'POST'])
+# @app.route('/artist/<name>', methods=['GET', 'POST'])
 def artist(name=""):
     if DEBUG:
         start = time.time()
-    print( "name is: " , name)
+    if request.method == 'POST':
+        result = request.form['name']
+    name = result
     payload = {'q': name, 'type': 'artist'}
-    r = requests.get('https://api.spotify.com/v1/search/', params=payload )
+    r = requests.get('https://api.spotify.com/v1/search/', params=payload)
     artist_id = r.json()['artists']['items'][0]['id']
     if DEBUG:
         end = time.time()
         total_time = end - start
         print('artist took {} seconds'.format(total_time))
-    return render_template('artist.html',name=name,id=artist_id ,image=get_artist_image(artist_id,image_num=1))
+    return render_template('artist.html',
+                           name=name,
+                           id=artist_id,
+                           image=get_artist_image(artist_id, image_num=1))
 
 
-@app.route('/artist/related-artists/<name>')
+@app.route('/related-artists/<name>')
 def related_artists(name):
+    print "received the name: {} from call".format(name)
     if DEBUG:
         start = time.time()
     artist_image = []
@@ -123,9 +126,17 @@ def related_artists(name):
         name_list.append(item['name'])
 
     preview_tracks = get_preview_tracks_async(preview_track_urls)
-    zipped_list = zip(name_list, related_artists_ids, artist_image,preview_tracks)
+    zipped_list = zip(name_list,
+                      related_artists_ids,
+                      artist_image, preview_tracks)
+    for id in related_artists_ids:
+        print("sending ID: {}").format(id)
+
     if DEBUG:
         end = time.time()
         total_time = end - start
         print('related_artists took {} seconds'.format(total_time))
-    return render_template('related-artist.html', name=name, id=related_artists_ids, related=zipped_list)
+    return render_template('related-artist.html',
+                           name=name,
+                           id=related_artists_ids,
+                           related=zipped_list)
