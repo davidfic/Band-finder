48fe4672d44e3ffd102a796ee2a7824395b87e94
David Ficociello <davidfic@gmail.com>
2018-01-13 00:34:58 -0500

    removed POST from endpoint

--- app/views.py
+++ app/views.py
@@ -104,7 +104,7 @@ def get_artist_image(artist_id, image_num=1):
 def index():
     return render_template('index.html')
 
-@app.route('/artist-id/<artist>', methods=['GET', 'POST'])
+@app.route('/artist-id/<artist>', methods=['GET'])
 def artist_id(artist):
     return get_artist_id(artist)
 
